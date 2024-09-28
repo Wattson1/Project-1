cee0f82061024e2571814fcf8604ef291c8c7ce7
Савин Иван <savin_ivan_061@mail.ru>
2024-09-28 22:51:37 +0300

    Initial Commit

---
+++ Project 1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project 1
+//
+//  Created by Ivan Savin on 28.09.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Project 1Tests/Project_1Tests.swift
@@ -0,0 +1,17 @@
+//
+//  Project_1Tests.swift
+//  Project 1Tests
+//
+//  Created by Ivan Savin on 28.09.2024.
+//
+
+import Testing
+@testable import Project_1
+
+struct Project_1Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
